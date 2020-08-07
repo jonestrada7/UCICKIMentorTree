1dea88fdab29bf5fb8a718775cd92800c1a131a0
natashza <trejonm@uci.edu>
2020-08-06 19:54:56 -0700

    updated lines

--- examples/arm_example/lines/MATCHA_LINE.js
+++ examples/arm_example/lines/MATCHA_LINE.js
@@ -4,61 +4,83 @@ var htmlclass = "matcha-line";
 
 var MATCHA_LINE = [
     {
-        text: { name: "Noah" },
-        image: file + "/img/ML1.jpg",
+        text: { name: "Omar" },
+        image: file + "/img/omar.jpg",
         HTMLclass: htmlclass,
         children: [
             {
-                text: { name: "Sydney" },
-                image: file + "/img/ML2.jpg",
-                HTMLclass: htmlclass,
-                children: [
-                    {
-                        text: { name: "Travis" },
-                        image: file + "/img/ML5.jpg",
-                        HTMLclass: htmlclass,
-                    },
-                    {
-                        text: { name: "Elton" },
-                        image: file + "/img/ML6.jpg",
-                        HTMLclass: htmlclass,
-                    }
-                ]
-            },
-            {
-                text: { name: "Ruhiyah" },
-                image: file + "/img/ML4.jpg",
+                text: { name: "Brittany" },
+                image: file + "/img/brittany.jpg",
                 HTMLclass: htmlclass
             },
             {
-                text: { name: "Kayla" },
-                image: file + "/img/ML3.jpg",
+                text: { name: "Noah" },
+                image: file + "/img/ML1.jpg",
                 HTMLclass: htmlclass,
                 children: [
                     {
-                        text: { name: "Justin" },
-                        image: file + "/img/ML7.jpg",
-                        HTMLclass: htmlclass
-                    },
-                    {
-                        text: { name: "Kit" },
-                        image: file + "/img/ML10.jpg",
+                        text: { name: "Sydney" },
+                        image: file + "/img/ML2.jpg",
                         HTMLclass: htmlclass,
                         children: [
                             {
-                                text: { name: "Tin" },
-                                image: file + "/img/ML8.jpg",
+                                text: { name: "Travis" },
+                                image: file + "/img/ML5.jpg",
                                 HTMLclass: htmlclass
                             },
                             {
-                                text: { name: "Nicole" },
-                                image: file + "/img/ML9.jpg",
+                                text: { name: "Elton" },
+                                image: file + "/img/ML6.jpg",
                                 HTMLclass: htmlclass
+                            }
+                        ]
+                    },
+                    {
+                        text: { name: "Ruhiyah" },
+                        image: file + "/img/ML4.jpg",
+                        HTMLclass: htmlclass
+                    },
+                    {
+                        text: { name: "Kasia" },
+                        image: file + "/img/kasia.jpg",
+                        HTMLclass: htmlclass
+                    },
+                    {
+                        text: { name: "Kayla" },
+                        image: file + "/img/ML3.jpg",
+                        HTMLclass: htmlclass,
+                        children: [
+                            {
+                                text: { name: "Justin" },
+                                image: file + "/img/ML7.jpg",
+                                HTMLclass: htmlclass
+                            },
+                            {
+                                text: { name: "Kit" },
+                                image: file + "/img/ML10.jpg",
+                                HTMLclass: htmlclass,
+                                children: [
+                                    {
+                                        text: { name: "Tin" },
+                                        image: file + "/img/ML8.jpg",
+                                        HTMLclass: htmlclass
+                                    },
+                                    {
+                                        text: { name: "Nicole" },
+                                        image: file + "/img/ML9.jpg",
+                                        HTMLclass: htmlclass
+                                    }
+                                ]
                             }
                         ]
                     }
                 ]
-            }   
-        ]
-    } 
+            },
+            {
+                text: { name: "Sophia" },
+                image: file + "/img/sophia_d.jpg",
+                HTMLclass: htmlclass
+            }
+        ]  
+    }
 ]
--- examples/arm_example/lines/WUJU_LINE.js
+++ examples/arm_example/lines/WUJU_LINE.js
@@ -1,21 +1,108 @@
-// var file = "/page/treant/treant-js-master/examples/arm_example";
+// let file = "/page/treant/treant-js-master/examples/arm_example";
 var file = "";      // FIXME: Be sure to change this to the above path when using the actual website
 var htmlclass = "wuju-line";
 
 var WUJU_LINE = [
     {
-        text: { name: "Andre" },
-        image: file + "/img/andre.jpg",
+        text: { name: "Jonathan" },
+        image: file + "img/jonathan.jpg",
         HTMLclass: htmlclass,
         children: [
             {
-                text: { name: "Ngan" },
-                image: file + "/img/ngan.jpg",
-                HTMLclass: htmlclass
+                text: { name: "Alison" },
+                image: "img/alison.jpg",
+                HTMLclass: htmlclass                
             },
             {
-                text: { name: "Andy" },
-                image: file + "/img/andy.jpg",
+                text: { name: "Matt"},
+                image: file + "img/matt.jpg",
+                HTMLclass: htmlclass,
+                children: [
+                    {
+                        text: { name: "Lianne" },
+                        image: file + "img/lianne.jpg",
+                        HTMLclass: htmlclass,
+                        children: [
+                            {
+                                text: { name: "Jake" },
+                                image: file + "img/jake.jpg",
+                                HTMLclass: htmlclass,
+                                children: [
+                                    {
+                                        text: { name: "Megumi" },
+                                        image: file + "/img/meg.jpg",
+                                        HTMLclass: htmlclass                                        
+                                    },
+                                    {
+                                        text: { name: "Albert" },
+                                        image: file + "/img/albert.jpg",
+                                        HTMLclass: htmlclass
+                                    }
+                                ],
+                            },
+                            {
+                                text: { name: "Ash" },
+                                image: file + "img/ash.jpg",
+                                HTMLclass: htmlclass
+                            },
+                            {
+                                text: { name: "Kaitlyn" },
+                                image: file + "/img/kaitlyn.jpg",
+                                HTMLclass: htmlclass
+                            }
+                        ]
+                    },
+                    {
+                        text: { name: "Kyle" },
+                        image: file + "/img/kyle_l.jpg",
+                        HTMLclass: htmlclass
+                    },
+                    {
+                        text: { name: "Andre" },
+                        image: file + "img/andre.jpg",
+                        HTMLclass: htmlclass,
+                        children: [
+                            {
+                                text: { name: "Ngan" },
+                                image: file + "img/ngan.jpg",
+                                HTMLclass: htmlclass,
+                                children: [
+                                    {
+                                        text: { name: "Amanda" },
+                                        image: file + "img/amanda.jpg",
+                                        HTMLclass: htmlclass
+                                    },
+                                    {
+                                        text: { name: "Julia" },
+                                        image: file + "img/julia.jpg",
+                                        HTMLclass: htmlclass
+                                    }
+                                ]
+                            },
+                            {
+                                text: { name: "Andy" },
+                                image: "img/andy.jpg",
+                                HTMLclass: htmlclass,
+                                children: [
+                                    {
+                                        text: { name: "Ana" },
+                                        image: file + "/img/ana.jpg",
+                                        HTMLclass: htmlclass
+                                    },
+                                    {
+                                        text: { name: "Sophia" },
+                                        image: file + "/img/sophia.jpg",
+                                        HTMLclass: htmlclass
+                                    }
+                                ]
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                text: { name: "Sophia" },
+                image: "img/sophia_h.jpg",
                 HTMLclass: htmlclass
             }
         ]
