063ff92fa9de3b0a1c4f60a4f71af3882a311a6d
jonestrada7 <jonathan.estrada747@gmail.com>
2020-08-06 22:50:04 -0700

    small update

--- examples/arm_example/treeconfig.js
+++ examples/arm_example/treeconfig.js
@@ -1,3 +1,5 @@
+console.log("testing");
+
 // var treePath = "/page/treant/treant-js-master/examples/arm_example";
 var treePath = "";      // FIXME: Be sure to change this to the above path when using the actual website
 
